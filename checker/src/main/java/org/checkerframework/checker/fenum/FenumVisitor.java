e7b20a9c90a6036dde0368ce7472b9bd9fd06765
d367wang <d367wang@uwaterloo.ca>
2020-09-30 03:59:22 -0400

    forbid binary operations except comparison

--- checker/src/main/java/org/checkerframework/checker/fenum/FenumVisitor.java
+++ checker/src/main/java/org/checkerframework/checker/fenum/FenumVisitor.java
@@ -6,10 +6,12 @@ import com.sun.source.tree.ExpressionTree;
 import com.sun.source.tree.NewClassTree;
 import com.sun.source.tree.SwitchTree;
 import com.sun.source.tree.Tree;
+import com.sun.source.tree.Tree.Kind;
 import java.util.Collections;
 import java.util.Set;
 import javax.lang.model.element.AnnotationMirror;
 import javax.lang.model.element.ExecutableElement;
+import org.checkerframework.checker.fenum.qual.Fenum;
 import org.checkerframework.common.basetype.BaseTypeChecker;
 import org.checkerframework.common.basetype.BaseTypeVisitor;
 import org.checkerframework.framework.type.AnnotatedTypeMirror;
@@ -25,21 +27,22 @@ public class FenumVisitor extends BaseTypeVisitor<FenumAnnotatedTypeFactory> {
 
     @Override
     public Void visitBinary(BinaryTree node, Void p) {
-        if (!TreeUtils.isStringConcatenation(node)) {
-            // TODO: ignore string concatenations
+        AnnotatedTypeMirror lhsAtm = atypeFactory.getAnnotatedType(node.getLeftOperand());
+        AnnotatedTypeMirror rhsAtm = atypeFactory.getAnnotatedType(node.getRightOperand());
 
-            // The Fenum Checker is only concerned with primitive types, so just check that
-            // the primary annotations are equivalent.
-            AnnotatedTypeMirror lhsAtm = atypeFactory.getAnnotatedType(node.getLeftOperand());
-            AnnotatedTypeMirror rhsAtm = atypeFactory.getAnnotatedType(node.getRightOperand());
-
+        if (TreeUtils.typeOf(node).getKind().isPrimitive() && isValidBinaryOperation(node)) {
+            // Fenum Checker only permits certain binary operations on primitive type
             Set<AnnotationMirror> lhs = lhsAtm.getEffectiveAnnotations();
             Set<AnnotationMirror> rhs = rhsAtm.getEffectiveAnnotations();
             QualifierHierarchy qualHierarchy = atypeFactory.getQualifierHierarchy();
             if (!(qualHierarchy.isSubtype(lhs, rhs) || qualHierarchy.isSubtype(rhs, lhs))) {
                 checker.reportError(node, "binary.type.incompatible", lhsAtm, rhsAtm);
             }
+
+        } else if (hasFenumAnnotation(lhsAtm) || hasFenumAnnotation(rhsAtm)) {
+            checker.reportError(node, "binary.operation.unsupported", lhsAtm, rhsAtm);
         }
+
         return super.visitBinary(node, p);
     }
 
@@ -87,5 +90,33 @@ public class FenumVisitor extends BaseTypeVisitor<FenumAnnotatedTypeFactory> {
         // modifier is FenumBottom, this results in an error when a non-subtype
         // is used. Can we use FenumTop as default instead?
         return true;
+    }
+
+    /**
+     * Check if the binary operation {@code BinaryTree} is valid. Fenum Checker forbids binary
+     * operations except comparison
+     *
+     * @param node the BinaryTree to test
+     * @return true if the BinaryTree represents valid binary expression
+     */
+    private boolean isValidBinaryOperation(BinaryTree node) {
+        Kind kind = node.getKind();
+        return kind == Tree.Kind.EQUAL_TO
+                || kind == Tree.Kind.NOT_EQUAL_TO
+                || kind == Tree.Kind.LESS_THAN
+                || kind == Tree.Kind.LESS_THAN_EQUAL
+                || kind == Tree.Kind.GREATER_THAN
+                || kind == Tree.Kind.GREATER_THAN_EQUAL;
+    }
+
+    /**
+     * Check if the given annotated type contains {@link Fenum} annotation
+     *
+     * @param atm {@link AnnotatedTypeMirror} to test
+     * @return true if {@code atm} contains {@link Fenum} annotation
+     */
+    private boolean hasFenumAnnotation(AnnotatedTypeMirror atm) {
+        AnnotationMirror anno = atm.getAnnotation(Fenum.class);
+        return anno != null;
     }
 }
